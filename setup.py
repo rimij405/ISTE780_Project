9cfd16037b9e24cc996c5caa6ece9eb145cc9405
Ian Effendi <effendiian@gmail.com>
2021-10-01 01:07:39 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Extraction and evaluation of Walmart product data features for classification and prediction of list price ranges.',
+    author='Ian Effendi',
+    license='MIT',
+)
